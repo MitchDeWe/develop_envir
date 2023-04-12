3e03a7238f822d612cc5aa9d0382cc8d17bdb414
Mitch De Weers <s4582695@student.uq.edu.au>
2023-04-12 23:18:09 +1000

    FInal streaming setup for recording

--- stream/main/app_main.cpp
+++ stream/main/app_main.cpp
@@ -5,7 +5,7 @@
 #include "app_mdns.h"
 #include "freertos/queue.h"
 
-static QueueHandle_t xQueueAIFrame = NULL;
+static QueueHandle_t xQueueStreamFrame = NULL;
 //static QueueHandle_t xQueueHttpFrame = NULL;
 
 extern "C" void app_main()
@@ -13,10 +13,10 @@ extern "C" void app_main()
     app_wifi_main();
     app_mdns_main();
 
-    xQueueAIFrame = xQueueCreate(2, sizeof(camera_fb_t *));
+    xQueueStreamFrame = xQueueCreate(2, sizeof(camera_fb_t *));
     //xQueueHttpFrame = xQueueCreate(2, sizeof(camera_fb_t *));
 
-    register_camera(PIXFORMAT_JPEG, FRAMESIZE_240X240, 2, xQueueAIFrame);
+    register_camera(PIXFORMAT_JPEG, FRAMESIZE_240X240, 2, xQueueStreamFrame);
     //register_human_face_detection(xQueueAIFrame, NULL, NULL, xQueueHttpFrame);
-    register_httpd(xQueueAIFrame, NULL, true);
+    register_httpd(xQueueStreamFrame, NULL, true);
 }
